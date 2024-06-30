108ff3c977b244ac7a28ea8ad41367bf8963eaa2
Jeevan-Neupane <jeevan.neupane003@gmail.com>
2024-06-30 06:37:31 +0545

    topo sort

--- Graph/week_02_month_03/topological sorting/topo_sort.cpp
+++ Graph/week_02_month_03/topological sorting/topo_sort.cpp
@@ -3,41 +3,67 @@
 #include <stack>
 using namespace std;
 
-vector<int> topoSort(int V, vector<int> adj[])
+void dfs(int start, vector<bool> &vis, stack<int> &s, vector<int> adj[])
 {
-    // code here
-    vector<int> vis(V, 0);
-    stack<int> st;
+    vis[start] = 1;
 
-    for (int i = 0; i < V; i++)
+    for (int nbr : adj[start])
     {
-        st.push(i);
-
-        while (!st.empty())
+        if (!vis[nbr])
         {
-            int node = st.top();
-            vis[node] = 1;
-
-            if (adj[node].size() == 0)
-            {
-                break;
-            }
-
-            st.pop();
-
-            for (auto it : adj[node])
-            {
-                if (vis[it] != 1)
-                {
-                    st.push(it);
-                }
-            }
+            dfs(nbr, vis, s, adj);
         }
     }
+
+    s.push(start);
 }
 
+vector<int> topoSort(int V, vector<int> adj[])
+{
+    vector<bool> vis(V);
+    stack<int> s;
+    for (int i = 0; i < V; i++)
+    {
+        if (!vis[i])
+        {
+            dfs(i, vis, s, adj);
+        }
+    }
+    vector<int> res;
+    for (int i = 0; i < V; i++)
+    {
+        res.push_back(s.top());
+        s.pop();
+    }
+    return res;
+}
 int main()
 {
+
+    //     6 8
+    // 6 3
+    // 7 3
+    // 6 5
+    // 3 5
+    // 7 1
+    // 1 2
+
+    int V = 8;
+    vector<int> adj[8];
+
+    adj[6].push_back(3);
+    adj[7].push_back(3);
+    adj[6].push_back(5);
+    adj[3].push_back(5);
+    adj[7].push_back(1);
+    adj[1].push_back(2);
+
+    vector<int> res = topoSort(V, adj);
+
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << res[i] << " ";
+    }
 
     return 0;
 }
